9b4b129229366a8ebed849670c19c7b1f9d4a635
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-07-29 04:07:25 +0200

    Add roots

---
+++ src/linalg_util.rs
@@ -0,0 +1,15 @@
+use nalgebra::DMatrix;
+use num_complex::Complex;
+
+use crate::Scalar;
+
+pub(crate) fn reverse_mut<T: Scalar>(mat: &mut DMatrix<Complex<T>>) {
+    let n = mat.shape().0;
+    for i in 0..n / 2 {
+        mat.swap_rows(i, n - i - 1);
+    }
+    let n = mat.shape().1;
+    for i in 0..n / 2 {
+        mat.swap_columns(i, n - i - 1);
+    }
+}
