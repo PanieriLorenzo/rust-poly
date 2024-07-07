ee11e6bd39a54a8e8248710579b522a70793292e
Lorenzo Panieri <panierilorenzo@gmail.com>
2024-07-07 17:25:43 +0200

    chore: removed unused deprecated internal `usize_to_scalar`

--- src/__util/casting.rs
+++ src/__util/casting.rs
@@ -1,7 +1,5 @@
 use num::cast;
 
-use crate::Scalar;
-
 pub(crate) fn usize_to_i32(x: usize) -> i32 {
     x.try_into().unwrap_or(i32::MAX)
 }
@@ -12,11 +10,4 @@ pub(crate) fn usize_to_u32(x: usize) -> u32 {
 
 pub(crate) fn usize_to_f64(x: usize) -> f64 {
     cast(x).unwrap_or(f64::INFINITY)
-}
-
-/// Convert usize to Scalar without any primitive casting
-#[deprecated(note = "use num::FromPrimitive::from_usize instead")]
-#[must_use]
-pub fn usize_to_scalar<T: Scalar>(x: usize) -> T {
-    T::from_usize(x).unwrap()
 }
